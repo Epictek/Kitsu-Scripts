689c90c191bdfc5b7a350eaea7cbb67aa09ec214
Kieran Coldron <kieran@coldron.com>
2015-04-04 17:35:09 +0100

    Create HummingbirdBlocker.user.js

---
+++ HummingbirdBlocker.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name        HummingbirdBlocker
+// @namespace   HummingbirdBlocker
+// @description HummingbirdBlocker
+// @updateURL https://github.com/Epictek/Hummingbird-scripts/HummingbirdBlocker.user.js
+// @downloadURL https://github.com/Epictek/Hummingbird-scripts/HummingbirdBlocker.user.js
+// @version     1
+// @run-at document-end
+// @grant		none
+
+// @match       *://hummingbird.me/*
+
+// ==/UserScript==
+
+var blocked = ["Akemi_Ayu"]
+
+function removePosts(){
+	$.each($('.story'), function(){
+		var title = $(this).find('.update-panel .user-info-bar .comment-content .story-title').text();
+		title = title.trim();
+		if(blocked.indexOf(title) > -1){
+	    	$(this).remove();
+		}
+	});
+	removeComments();
+}
+
+function removeComments () {
+	$.each($('.reply'), function(){
+		var user = $(this).find('.username').text();
+		console.log(user);
+		user = user.trim();
+		if(blocked.indexOf(user) > -1){
+	    	$(this).remove();
+		}
+	});
+}
+
+setInterval(function() {
+	removePosts();
+}, 300);
+
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+       removePosts();
+   }
+});
