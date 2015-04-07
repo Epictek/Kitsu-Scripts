f0edf4ee8ac7c789f90e843b93adebc2ae009098
Epictek <kieran@coldron.com>
2015-04-07 14:26:42 +0100

    Removed uneeded code

--- HummingbirdBlocker.user.js
+++ HummingbirdBlocker.user.js
@@ -14,27 +14,24 @@
 
 var blocked = ["Akemi_Ayu"];
 
-function removePosts(){
+function removeStuff(){
 	$.each($('.story'), function(){
 		var title = $(this).find('.update-panel .user-info-bar .comment-content .story-title').text();
 		title = title.trim();
 		if(blocked.indexOf(title) > -1){
-	    	$(this).remove();
+			$(this).remove();
 		}
 	});
-	removeComments();
-}
-
-function removeComments () {
 	$.each($('.reply'), function(){
 		var user = $(this).find('.username').text();
 		user = user.trim();
 		if(blocked.indexOf(user) > -1){
-	    	$(this).remove();
+			$(this).remove();
 		}
 	});
 }
 
+//If someone knows a better way to hangle ember route changes please let me know!
 setInterval(function() {
-	removePosts();
+	removeStuff();
 }, 300);
